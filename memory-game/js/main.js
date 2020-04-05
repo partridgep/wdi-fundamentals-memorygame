49317ebec7445c0de3254c0482f90d4a2aac9429
Paul <paulpartridge@MacBook-Paul-2.local>
2020-04-05 16:59:06 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -4,25 +4,37 @@ let cards = ['queen', 'queen', 'king', 'king'];
 //array of cards that are in play, empty at first
 let cardsInPlay = [];
 
-//selecting first and third cards
-let cardOne = cards[0];
-let cardTwo = cards[2];
-
-//pushing selected cards into array of cards in play
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log(cardOne);
-console.log(cardTwo);
-
-//checking if player picked two cards
-if (cardsInPlay.length === 2) {
-	//checking if two cards match
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-}
-else {
+//checking if two cards match
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+  }
+  else {
 	//if they don't match
-	alert("Sorry, try again.")
+	console.log("Sorry, try again.");
+  }
 }
-}			
+
+//all steps that should happen when the user flips a card
+function flipCard(cardId) {
+  console.log("User flipped "+cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  //checking if player picked two cards
+  if (cardsInPlay.length === 2) {
+  checkForMatch();
+  }
+}
+
+//simulating player flipping 2 cards
+flipCard(0);
+flipCard(2);
+
+
+  
+ 	 
+
+
+
+
+
+	
