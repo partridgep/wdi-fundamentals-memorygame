c7bdc9481725961f30f03339fd78ef217dcc5adf
Paul <paulpartridge@MacBook-Paul-2.local>
2020-04-05 18:01:17 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,26 @@
 //creating array of cards
-let cards = ['queen', 'queen', 'king', 'king'];
+let cards = [
+  {
+  	rank: "queen",
+  	suit: "hearts",
+  	cardImage: "images/queen-of-hearts.png"
+  },
+  {
+  	rank: "queen",
+  	suit: "diamonds",
+  	cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+  	rank: "king",
+  	suit: "hearts",
+  	cardImage: "images/king-of-hearts.png"
+  },
+  {
+  	rank: "king",
+  	suit: "diamonds",
+  	cardImage: "images/king-of-diamonds.png"
+  }
+];
 
 //array of cards that are in play, empty at first
 let cardsInPlay = [];
@@ -7,18 +28,20 @@ let cardsInPlay = [];
 //checking if two cards match
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	alert("You found a match!");
   }
   else {
 	//if they don't match
-	console.log("Sorry, try again.");
+	alert("Sorry, try again.");
   }
 }
 
 //all steps that should happen when the user flips a card
 function flipCard(cardId) {
-  console.log("User flipped "+cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped "+cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
   //checking if player picked two cards
   if (cardsInPlay.length === 2) {
   checkForMatch();
